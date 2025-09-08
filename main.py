f3dd0f899017c103a83965f874773acab357310c
dor-xl <dedenglieur@gmail.com>
2025-09-08 18:12:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def main():
                 get_packages_by_family(family_code)
                 continue
             elif choice == "7":
-                # <<< Tambahan: Ganti Tema >>>
+                # Ganti Tema
                 change_theme_menu()
                 continue
             elif choice == "00":
