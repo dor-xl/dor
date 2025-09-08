6378a2465f71b4530c2029b6f02733f2212ea50e
dor-xl <dedenglieur@gmail.com>
2025-09-08 17:00:29 +0700

    Update paket_custom_family.py

--- paket_custom_family.py
+++ paket_custom_family.py
@@ -16,6 +16,7 @@ try:
     from rich.table import Table
     from rich.panel import Panel
     from rich.prompt import Prompt
+    from rich.align import Align
     from rich.box import ROUNDED
 except ImportError:
     pass
